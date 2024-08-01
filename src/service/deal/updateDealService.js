359aa09532a1deef7a78508b9deb6cb3c7612a53
Heitor Santos <heitorg33@gmail.com>
2024-08-01 17:22:29 -0300

    Refactoring deal!

--- src/service/deal/updateDealService.js
+++ src/service/deal/updateDealService.js
@@ -15,14 +15,10 @@ const { searchProducts } = require("../../model/product/searchproductModel");
 const updateDealService = async (body) => {
   const { produtos } = body;
 
-  console.log("Update 01/08/2024: ",body)
-
   const responseIdQuote = await searchDeal(body);
 
-
-  console.log("Response ID Quote", responseIdQuote)
-  if(!responseIdQuote){
-    return `Não encontramos o negócio com o ID: ${body.id_da_cotacao}`
+  if (!responseIdQuote) {
+    return `Não encontramos o negócio com o ID: ${body.id_da_cotacao}`;
   }
 
   try {
@@ -53,13 +49,9 @@ const updateDealService = async (body) => {
         await updateDeal(id, data);
       }
 
-      console.log("Items de linha", responseLineItems);
-
       if (responseLineItems.length !== 0 && responseLineItems) {
         await Promise.all(
           responseLineItems.map(async (item) => {
-            console.log(item.id);
-            console.log("ID deal", id);
             const url = `crm/v4/objects/deals/${id}/associations/line_items/${item.id}`;
             await deletePropertiesHS(url);
           })
@@ -69,11 +61,8 @@ const updateDealService = async (body) => {
       const productsAssociateds = [];
       const existingProducts = await searchProducts(produtos);
 
-      console.log("Existe Produtos", existingProducts);
-
       if (existingProducts.length !== produtos.length || !existingProducts) {
         if (existingProducts.length === 0 || !existingProducts) {
-          console.log("Existe Produtos", existingProducts);
           for (const product of produtos) {
             const {
               properties: {
@@ -150,7 +139,6 @@ const updateDealService = async (body) => {
               };
               productsAssociateds.push(dataAssociates);
             } else {
-              console.log("Produto já cadastrado", product);
               const { sku_mais_pratico, quantidade, price, nomeDoProduto } =
                 product;
               let productId;
@@ -199,12 +187,6 @@ const updateDealService = async (body) => {
           );
           let productId;
           existingProducts.forEach(({ properties }) => {
-            console.log(
-              "PROPERTIES",
-              properties,
-              "SKU+PRATICO",
-              sku_mais_pratico
-            );
             Number(properties.sku_mais_pratico) === Number(sku_mais_pratico)
               ? (productId = properties.hs_object_id)
               : productId;
@@ -242,7 +224,7 @@ const updateDealService = async (body) => {
           await createLineItem(properties);
         })
       );
-      console.log("Update Quote", responseIdQuote);
+
       return {
         message: "Negócio Atualizado com Sucesso!",
         dealName: responseIdQuote[0].properties.dealname,
@@ -252,88 +234,5 @@ const updateDealService = async (body) => {
   } catch (error) {
     return error.message;
   }
-  
-
-  // if (produtos.length > 0) {
-  //   if (responseLineItems.length === produtos.length) {
-  //     for (const { sku_mais_pratico, price, quantidade } of produtos) {
-  //       responseLineItems.forEach(async ({ properties }) => {
-  //         if (
-  //           Number(sku_mais_pratico) === Number(properties.sku_mais_pratico)
-  //         ) {
-  //           const data = {
-  //             properties: {
-  //               price,
-  //               quantity: quantidade,
-  //             },
-  //           };
-  //           await updateLineItem(properties.hs_object_id, data);
-  //           return "Negócio Atualizado com Sucesso!";
-  //         }
-  //       });
-  //     }
-  //   } else {
-  //     for (const product of produtos) {
-  //       let lineItemId;
-  //       let itemNotDefined;
-  //       responseLineItems.forEach(({ properties }) => {
-  //         properties.sku_mais_pratico === product.sku_mais_pratico
-  //           ? (lineItemId = properties.hs_object_id)
-  //           : (itemNotDefined = properties.hs_object_id);
-  //       });
-  //       if (lineItemId) {
-  //         console.log(lineItemId);
-  //         const { price, quantidade } = product;
-  //         const data = {
-  //           properties: {
-  //             price,
-  //             quantity: quantidade,
-  //           },
-  //         };
-  //         await updateLineItem(lineItemId, data);
-  //         return "Negócio Atualizado com Sucesso!";
-  //       } else {
-  //         const {
-  //           properties: {
-  //             quantidade,
-  //             price,
-  //             hs_object_id,
-  //             name,
-  //             sku_mais_pratico,
-  //           },
-  //         } = await createProduct(product);
-  //         const dataAssociates = {
-  //           properties: {
-  //             quantity: quantidade,
-  //             price,
-  //             hs_product_id: hs_object_id,
-  //             name,
-  //             sku_mais_pratico,
-  //           },
-  //           associations: [
-  //             {
-  //               to: {
-  //                 id: id,
-  //               },
-  //               types: [
-  //                 {
-  //                   associationCategory: "HUBSPOT_DEFINED",
-  //                   associationTypeId: 20,
-  //                 },
-  //               ],
-  //             },
-  //           ],
-  //         };
-
-  //         await createLineItem(dataAssociates);
-  //         return { message: "Negócio Atualizado com Sucesso!" };
-  //       }
-  //     }
-  //   }
-  // }
-
-  /**Se quantidade de produto for diferente, verificar qual é a diferença e fazer associações */
-
-  /**Verificar se todos os produtos são iguais */
 };
 module.exports = { updateDealService };
